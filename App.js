4bae867df3863a18a224b7ecfd065da3a6d1f622
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-03-07 00:23:57 +0800

    Initital commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>CoinGecko Clone</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
