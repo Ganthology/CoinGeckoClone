023eb88202c2d538c22a9974d965f05ff8b6382b
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-03-08 00:34:21 +0800

    Add image assets

---
+++ assets/images/index.js
@@ -0,0 +1,21 @@
+import CoinGeckoLogoDark from './coingecko-dark.svg';
+import CoinGeckoLogoLight from './coingecko-light.svg';
+import AddButtonDark from './add-button-dark.png';
+import AddButtonLight from './add-button-light.png';
+import BellButtonDark from './bell-button-dark.png';
+import BellButtonLight from './bell-button-light.png';
+import SearchButtonDark from './search-button-dark.png';
+import SearchButtonLight from './search-button-light.png';
+import Candy from './candy.svg';
+
+export {
+  CoinGeckoLogoDark,
+  CoinGeckoLogoLight,
+  AddButtonDark,
+  AddButtonLight,
+  BellButtonDark,
+  BellButtonLight,
+  SearchButtonDark,
+  SearchButtonLight,
+  Candy,
+};
