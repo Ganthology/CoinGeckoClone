6fda83346af37ecf081f5570f62a8822ada106a0
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-03-14 13:29:49 +0800

    Refactor styles in SortingPill component

--- components/market/SortingPill.js
+++ components/market/SortingPill.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {WIDTH} from '../../utils';
 
 const SortingPill = ({title}) => {
   return (
-    <View
-      style={{
-        backgroundColor: 'rgba(81,81,81,0.7)',
-        paddingVertical: 5,
-        paddingHorizontal: 15,
-        borderRadius: 20,
-        marginRight: WIDTH * 0.025,
-      }}>
-      <Text
-        style={{
-          color: 'white',
-          fontWeight: '600',
-        }}>
-        {title}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
 
 export default SortingPill;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(81,81,81,0.7)',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    borderRadius: 20,
+    marginRight: WIDTH * 0.025,
+  },
+  text: {
+    color: 'white',
+    fontWeight: '600',
+  },
+});
